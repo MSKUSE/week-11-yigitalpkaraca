b998d077fb2791a83d4c03fd1c9bb694ec0b166f
yigitalpkaraca <yigitalpkaraca@posta.mu.edu.tr>
2025-05-22 12:54:34 +0300

    week 11 uploaded

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
 
         Stack stack = new StackLinkedList();
-        stack.push("This is a string");
-        stack.push(5);
-        stack.peek();
-        System.out.println("Is empty "
-        +stack.isEmpty() );
-        System.out.println("Pop = " +
-                stack.pop());
-        stack.peek();
-    }
+        try{
+            stack.pop();
+        }catch (Exception e ){
+            System.out.println(e);
+
+        }
+
+       /* ArrayList<Object> stringArrayList = new ArrayList<>();
+        stringArrayList.add(123);
+
+        HashMap<String, Double> grades = new HashMap<>();
+
+        grades.put("Ali", 35.0);
+        System.out.println(grades);
+
+        grades.put("Pelin", 90.0);
+        System.out.println(grades);
+        grades.put("Enes",10.0);
+        System.out.println(grades);
+        grades.put("Ali",60.0);
+        System.out.println(grades);
+        System.out.println(grades.keySet() instanceof Set);*/
+}
 }
